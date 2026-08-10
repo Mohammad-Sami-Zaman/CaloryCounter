6f52c07a3a9146077d02a29bc58cb05d390845ab
Mohammad Sami Zaman <mohammad.sami.zaman@g.bracu.ac.bd>
2026-08-10 21:41:23 +0600

    yes

--- Sami_07_CaloryCounter/settings.py
+++ Sami_07_CaloryCounter/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,10 @@ SECRET_KEY = 'django-insecure-ous9cu(v&$k=x2!zndn7idw%^+v!##p1hnl&h@r2-hal*mmyh8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
