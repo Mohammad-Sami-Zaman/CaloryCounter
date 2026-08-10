f2548ec18d54e21d67de6e587167938fd50516a5
Mohammad Sami Zaman <mohammad.sami.zaman@g.bracu.ac.bd>
2026-08-10 21:17:05 +0600

    first commit

---
+++ calory_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from calory_app.models import *
+
+admin.site.register([customUser, profileModel, consumedColorieModel])
---
+++ calory_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CaloryAppConfig(AppConfig):
+    name = 'calory_app'
